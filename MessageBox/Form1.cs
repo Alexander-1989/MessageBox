514c4a529872c52c2635484bac188be294c748bc
Alexander-1989 <eleventh_route@mail.ru>
2023-08-09 07:31:28 +0300

    Add project files.

---
+++ MessageBox/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MessageBoxShow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MsgBox msgBox = new MsgBox("Hello! I'm Message Box!");
+            msgBox.Show(this);
+        }
+    }
+}
